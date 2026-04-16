6416e88e42211381ee409c82478aa862f42bcf28
Thiago-1203 <alumno27.andreoli.castro.thiago@ipm.edu.ar>
2026-04-16 12:01:29 -0300

    Ej 11 y 10 hechos pero el 11 esta mal

--- Procedures_y_Cursor/EjerciciosCursor.sql
+++ Procedures_y_Cursor/EjerciciosCursor.sql
@@ -26,7 +26,65 @@ SELECT @lista;
 /*10*/
 
 delimiter // 
-CREATE PROCEDURE insertCancelled
+CREATE PROCEDURE insertCancelled(out cant int)
+BEGIN
+	declare hayFilas boolean;
+    declare ordenCancelada varchar(45) default "";
+    declare ordersCursor cursor for select ord.status from orders ord;
+	declare continue handler for not found set hayFilas = 0;
+    set cant = 0;
+    open ordersCursor;
+    ordersLoop:loop
+		fetch ordersCursor into ordenCancelada;
+        if hayFilas = 0 then
+			leave ordersLoop;
+		end if;
+        if(ordenCancelada = "Cancelled") then
+        set cant = cant + 1;
+        end if;
+        end loop ordersLoop;
+        close ordersCursor;
+end//
+delimiter ;
 
+call insertCancelled(@cant);
+select @cant;
+
+/*11*/
+delimiter //
+CREATE PROCEDURE alterCommentOrder(in idCl int)
+BEGIN
+	declare hayFilas boolean;
+    declare completarCommentario text default "";
+    declare totalOrden int;
+    declare ordenCursor cursor for select ord.comments from orders ord;
+    declare continue handler for not found set hayFilas = 0;
+    
+    SELECT sum(ordd.quantityOrdered * ordd.priceEach) into totalOrden from orderdetails ordd
+    JOIN orders ord ON ord.orderNumber = ordd.orderNumber
+    WHERE ord.customerNumber = idCl;
+    
+    open ordenCursor;
+    ordenLoop:loop
+		fetch ordenCursor into completarCommentario;
+			if hayFilas = 0 then
+				leave ordenLoop;
+			end if;
+            if completarCommentario = NULL then
+				Update orders
+					SET comments = concat("El total de la orden es", totalOrden)
+					WHERE customerNumber = idCl;
+			end if;
+            end loop ordenLoop;
+            close ordenCursor;
+end // 
+delimiter ;
+
+drop procedure alterCommentOrder;
+
+call alterCommentOrder(121);
+
+    
+        
 
     
