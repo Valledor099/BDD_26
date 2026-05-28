367cbfc52c9ebd1fa43f36c197a338b281254745
Thiago-1203 <alumno27.andreoli.castro.thiago@ipm.edu.ar>
2026-05-28 12:01:23 -0300

    Ej de transacciones

---
+++ Transacciones/ejercicio_classicmodels.sql
@@ -0,0 +1,28 @@
+USE classicmodels;
+
+-- 1 -- 
+DELIMITER //
+create procedure compra (in idcliente int, in idproducto text, in cantidad int, in fechaenvio date)
+begin 
+start transaction;
+update products set quantityInStock = quantityInStock - cantidad
+where productCode = idproducto;
+if(select quantityInStock from products where productCode = idproducto) < 0 then 
+rollback;
+signal sqlstate '45000' set MESSAGE_TEXT = 'Error, saldo insuficiente';
+end if;
+commit;
+end//
+DELIMITER ;
+
+drop procedure compra;
+
+CALL compra (12,'S10_1678', 10000,date(now()))
+
+-- 3 --
+delimiter //
+create procedure cambiaEstado (in numeroPedido int)
+begin
+
+
+
